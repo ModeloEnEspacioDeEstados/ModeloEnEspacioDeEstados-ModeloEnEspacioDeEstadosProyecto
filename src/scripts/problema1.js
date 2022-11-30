b18c708c703f09dd2b4eacdb8ec800c01a9a0a64
ModeloEnEspacioDeEstados <119541782+ModeloEnEspacioDeEstados@users.noreply.github.com>
2022-11-30 15:54:51 -0600

    Add files via upload

---
+++ src/scripts/problema1.js
@@ -0,0 +1,72 @@
+
+const btnResolverProblema = document.querySelector("#resolverProblema");
+btnResolverProblema.addEventListener('click',(e)=>{
+
+    const valorR = document.querySelector("#r").value;
+    const valorC= document.querySelector("#c").value;
+    
+    const r = Number(valorR); 
+    const c  = Number(valorC);
+    
+    let rc;
+    if (valorC!="" || valorR!="") {
+        if (isNaN(r)|| isNaN(c))   {
+            Swal.fire({
+                icon: 'error',
+                title: 'VERIFIQUE LOS DATOS',
+                text: 'Los datos ingresados no son números',
+                
+              })
+        }else{
+          if (r==0 || c==0) {
+            Swal.fire({
+                icon: 'error',
+                title: 'VERIFIQUE LOS DATOS',
+                text: 'No se puede dividir entre 0',
+                
+              })
+            
+        } else{
+            const corchetes = document.querySelectorAll("#corchete");
+            while (corchetes[0].firstChild) {
+                corchetes[0].removeChild(corchetes[0].lastChild);
+              }
+
+              while (corchetes[1].firstChild) {
+                corchetes[1].removeChild(corchetes[1].lastChild);
+              }
+            
+            rc  = 1/ (r*c);
+           corchetes[0].innerHTML=  `
+           <mrow>
+           <mi id="RC">${-1*(rc)}</mi>
+            </mrow>
+           
+           `;
+           corchetes[1].innerHTML=`<mi id="RC">${(rc)}</mi>`;
+           
+        }
+          
+        }
+        
+       
+        
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'VERIFIQUE LOS DATOS',
+            text: 'El campo está vacio',
+            
+          })
+    }
+
+    
+
+   
+
+
+
+    
+
+
+})
